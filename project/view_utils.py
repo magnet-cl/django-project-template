dd5f9a303dffaf7cbac425c91572ca59c3ff5263
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2017-05-26 19:23:13 -0400

    Add Base classed based views

---
+++ project/view_utils.py
@@ -0,0 +1,40 @@
+# django
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+# standard library
+
+
+def paginate(request, objects, page_size=25):
+    paginator = Paginator(objects, page_size)
+    page = request.GET.get('p')
+
+    try:
+        paginated_objects = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        paginated_objects = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        paginated_objects = paginator.page(paginator.num_pages)
+
+    return paginated_objects
+
+
+def clean_query_string(request):
+    clean_query_set = request.GET.copy()
+
+    clean_query_set = dict(
+        (k, v) for k, v in request.GET.items() if not k.startswith('o_')
+    )
+
+    try:
+        del clean_query_set['p']
+    except:
+        pass
+
+    mstring = []
+    for key in clean_query_set.iterkeys():
+        valuelist = request.GET.getlist(key)
+        mstring.extend(['%s=%s' % (key, val) for val in valuelist])
+
+    return '&'.join(mstring)
--- project/views.py
+++ project/views.py
@@ -1,12 +1,158 @@
 # -*- coding: utf-8 -*-
-""" This file contains some generic views """
+""" This file contains some generic purpouse views """
+
+# standard library
 
 # django
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.core.exceptions import PermissionDenied
+from django.shortcuts import get_object_or_404
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.utils.decorators import method_decorator
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.views.generic.edit import DeleteView
+from django.views.generic.edit import UpdateView
+from django.views.generic.list import ListView
+
+# utils
+from project.view_utils import clean_query_string
+from project.utils import camel_to_underscore
 
 
 @login_required
 def index(request):
     """ view that renders a default home"""
-    return render(request, 'index.pug')
+    return render_to_response('index.pug',
+                              context_instance=RequestContext(request))
+
+
+def bad_request_view(request):
+    return render_to_response('exceptions/400.jade', {},
+                              context_instance=RequestContext(request))
+
+
+def permission_denied_view(request):
+    return render_to_response('exceptions/403.jade', {},
+                              context_instance=RequestContext(request))
+
+
+def page_not_found_view(request):
+    return render_to_response('exceptions/404.jade', {},
+                              context_instance=RequestContext(request))
+
+
+def error_view(request):
+    return render_to_response('exceptions/500.jade', {},
+                              context_instance=RequestContext(request))
+
+
+class PermissionRequiredMixin:
+    permission_required = None
+
+    def check_permission_required(self):
+        if self.permission_required:
+            if not self.request.user.has_perm(self.permission_required):
+                raise PermissionDenied
+
+
+class BaseDetailView(DetailView, PermissionRequiredMixin):
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        self.check_permission_required()
+        return super(BaseDetailView, self).dispatch(*args, **kwargs)
+
+
+class BaseCreateView(CreateView, PermissionRequiredMixin):
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        self.check_permission_required()
+        return super(BaseCreateView, self).dispatch(*args, **kwargs)
+
+
+class BaseSubModelCreateView(CreateView, PermissionRequiredMixin):
+    """
+    Create view when the object is nested within a parent object
+    """
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        self.check_permission_required()
+        return super(BaseSubModelCreateView, self).dispatch(*args, **kwargs)
+
+    def get_form_kwargs(self):
+        model_underscore_name = camel_to_underscore(self.parent_model.__name__)
+
+        obj = get_object_or_404(
+            self.parent_model,
+            pk=self.kwargs['{}_id'.format(model_underscore_name)]
+        )
+
+        self.object = self.model(**{model_underscore_name: obj})
+
+        return super(BaseSubModelCreateView, self).get_form_kwargs()
+
+    def get_context_data(self, **kwargs):
+        context = super(BaseSubModelCreateView, self).get_context_data(
+            **kwargs
+        )
+        model_underscore_name = camel_to_underscore(self.parent_model.__name__)
+
+        obj = get_object_or_404(
+            self.parent_model,
+            pk=self.kwargs['{}_id'.format(model_underscore_name)]
+        )
+
+        context[model_underscore_name] = obj
+
+        return context
+
+
+class BaseListView(ListView, PermissionRequiredMixin):
+    paginate_by = 25
+    page_kwarg = 'p'
+
+    def get_ordering(self):
+        """
+        Return the field or fields to use for ordering the queryset.
+        """
+        order = self.request.GET.get('_o')
+        if order:
+            return (order,)
+
+        return self.ordering
+
+    def get_context_data(self, **kwargs):
+        context = super(BaseListView, self).get_context_data(**kwargs)
+        context['clean_query_string'] = clean_query_string(self.request)
+        context['q'] = self.request.GET.get('q')
+        return context
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        self.check_permission_required()
+        return super(BaseListView, self).dispatch(*args, **kwargs)
+
+
+class BaseUpdateView(UpdateView, PermissionRequiredMixin):
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        self.check_permission_required()
+        return super(BaseUpdateView, self).dispatch(*args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(BaseUpdateView, self).get_context_data(**kwargs)
+
+        context['cancel_url'] = self.object.get_absolute_url()
+
+        return context
+
+
+class BaseDeleteView(DeleteView, PermissionRequiredMixin):
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        self.check_permission_required()
+        return super(BaseDeleteView, self).dispatch(*args, **kwargs)
