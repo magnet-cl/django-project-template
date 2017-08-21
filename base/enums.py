4d7d45c4ebd1ce27379fead97bd34d8e7ac15dc3
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2017-08-21 11:49:01 -0300

    Add ChoieceEnum class for handling django enums

---
+++ base/enums.py
@@ -0,0 +1,14 @@
+import inspect
+from enum import Enum
+
+
+class ChoiceEnum(Enum):
+    @classmethod
+    def choices(cls):
+        # get all members of the class
+        members = inspect.getmembers(cls, lambda m: not(inspect.isroutine(m)))
+        # filter down to just properties
+        props = [m for m in members if not(m[0][:2] == '__')]
+        # format into django choice tuple
+        choices = tuple([(str(p[1].value), p[0]) for p in props])
+        return choices
