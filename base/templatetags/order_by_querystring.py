e88d12fb22f28adc5b7050ef1ffeb2bdbd829b52
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2017-09-25 03:28:59 -0300

    Add better comments to order_by_querystring template tag

--- base/templatetags/order_by_querystring.py
+++ base/templatetags/order_by_querystring.py
@@ -4,15 +4,19 @@ register = template.Library()
 
 
 @register.simple_tag
-def get_order_by_querytring(**kwargs):
+def get_order_by_querytring(ordering, current_order=None, remove=False):
+    """
+    Using the ordering parameter (a list), returns a query string with the
+    orders of the columns
 
-    '''
-    returns a remove user url from an object, based on the context
-    '''
+    The parameter current_order can be passed along to handle the specific
+    order of a single column. So for example if you are ordering by 'email' and
+    'first_name', you can pass on 'email' as the current order, so the system
+    can keep every other order, but inverse the order of the email field.
+    """
 
-    ordering = kwargs['ordering']
-    current_order = kwargs['current_order']
-    remove = kwargs.get('remove')
+    if not current_order:
+        return '&'.join(['o={}'.format(o) for o in ordering])
 
     reversed_current_order = '-{}'.format(current_order)
 
