eeeb1da7e9e58569d6cf72aadd2f912fabe04d10
Cristian Sepulveda <cristian@magnet.cl>
2017-10-24 09:39:32 -0300

    add media urls in project urls.py

--- project/urls.py
+++ project/urls.py
@@ -17,6 +17,7 @@ from django.conf import settings
 from django.conf.urls import include
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls.static import static
 
 from base import views as base_views
 
@@ -33,4 +34,7 @@ if settings.DEBUG:
     import debug_toolbar
     urlpatterns = [
         url(r'^__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
+    ] + urlpatterns + static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
