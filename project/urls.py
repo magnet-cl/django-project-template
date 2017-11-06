da0c9358fc3d38d5519f6f5940f083eea8faa9b4
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2017-11-06 12:44:05 -0300

    Delete roles urls from project urls

--- project/urls.py
+++ project/urls.py
@@ -25,7 +25,6 @@ urlpatterns = [
     url(r'^admin/', include('loginas.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('users.urls')),
-    url(r'^roles/', include('roles.urls')),
     url(r'^$', base_views.index, name='home'),
 ]
 
