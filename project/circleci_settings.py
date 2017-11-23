480b0bd80f65646da52824403ade92880af1af2e
Joao S. López <jslopez@csrg.inf.utfsm.cl>
2017-11-23 18:38:26 -0300

    Add circle ci settings

---
+++ project/circleci_settings.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+DEBUG = True
+
+LOCAL_DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'circle_test',
+        'USER': 'circleci',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+LOCALLY_INSTALLED_APPS = [
+]
+
+ENABLE_EMAILS = False
+
+LOCALLY_ALLOWED_HOSTS = [
+]
+
+ADMINS = []
