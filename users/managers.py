fa254862384e21e1c4a4f60ee3789db5da4a58c4
Jorge Guerra <jfguerra00@gmail.com>
2018-06-21 13:12:00 -0400

    Fix user manager create user and superuser methods

--- users/managers.py
+++ users/managers.py
@@ -24,24 +24,32 @@ class UserQuerySet(QuerySet):
 
 
 class UserManager(BaseUserManager):
-    def _create_user(self, username, email, password, **extra_fields):
+    def _create_user(self, email, first_name, last_name,
+                     password, **extra_fields):
         """
         Creates and saves a User with the given username, email and password.
         """
         if not email:
             raise ValueError('The given email must be set')
         email = self.normalize_email(email)
-        user = self.model(username=username, email=email, **extra_fields)
+        user = self.model(
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            **extra_fields
+        )
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_user(self, email=None, password=None, **extra_fields):
+    def create_user(self, email=None, first_name=None, last_name=None,
+                    password=None, **extra_fields):
         extra_fields.setdefault('is_staff', False)
         extra_fields.setdefault('is_superuser', False)
         return self._create_user(email, password, **extra_fields)
 
-    def create_superuser(self, email, password, **extra_fields):
+    def create_superuser(self, email, password, first_name=None,
+                         last_name=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
