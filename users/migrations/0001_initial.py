826fe3e9bfb7ed7c93615c1c9c58d24b8d5ea96b
Ignacio Munizaga <ignaciomunizaga@Ignacios-MacBook-Pro.local>
2019-03-07 18:58:56 -0300

    Fix regions and users migrations
    * there was a problem with the verbose names on updated and created at
    fields

--- users/migrations/0001_initial.py
+++ users/migrations/0001_initial.py
@@ -23,7 +23,7 @@ class Migration(migrations.Migration):
                 ('last_login', models.DateTimeField(blank=True, null=True, verbose_name='last login')),
                 ('is_superuser', models.BooleanField(default=False, help_text='Designates that this user has all permissions without explicitly assigning them.', verbose_name='superuser status')),
                 ('created_at', models.DateTimeField(auto_now_add=True, help_text='creation date', verbose_name='created at')),
-                ('updated_at', models.DateTimeField(auto_now=True, help_text='edition date', null=True, verbose_name='created at')),
+                ('updated_at', models.DateTimeField(auto_now=True, help_text='edition date', null=True, verbose_name='updated at')),
                 ('email', models.EmailField(db_index=True, max_length=254, unique=True, verbose_name='email address')),
                 ('first_name', models.CharField(blank=True, max_length=30, verbose_name='first name')),
                 ('last_name', models.CharField(blank=True, max_length=30, verbose_name='last name')),
