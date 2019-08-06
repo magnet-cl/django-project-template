6db4f8b1784023143ab8a2ab2f16e20847058a86
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2019-08-06 13:19:48 -0400

    Add timeout of 1 seconds for webpack compialtion

--- project/settings.py
+++ project/settings.py
@@ -369,7 +369,7 @@ WEBPACK_LOADER = {
         "BUNDLE_DIR_NAME": "bundles/",
         "STATS_FILE": os.path.join(BASE_DIR, "webpack-stats.json"),
         "POLL_INTERVAL": 0.1,
-        "TIMEOUT": None,
+        "TIMEOUT": 1, # 1 second timeout for webpack compilation
         "IGNORE": [r'.+\.hot-update.js', r'.+\.map']
     }
 }
