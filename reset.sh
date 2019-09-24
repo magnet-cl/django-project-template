2ccd33b0e5631ad95d93285a10e2d4810be37214
taandrighetti <taandrighetti@uc.cl>
2019-09-24 17:54:51 -0300

    chore: improve and simplify reset.sh

--- reset.sh
+++ reset.sh
@@ -1,45 +1,22 @@
 #!/bin/bash
+set -e
 
-RUNSERVER=false
-while getopts “sh” OPTION
-do
-    case $OPTION in
-        s)
-             echo "Start Server"
-             RUNSERVER=true
-             ;;
-        ?)
-             echo "fail"
-             exit
-             ;;
-     esac
-done
+function print_red() {
+    echo -e "\033[31m$1\033[39m"
+}
 
-engine=`python -c"from project.settings import LOCAL_DATABASES; print(LOCAL_DATABASES['default']['ENGINE'])"`
-debug=`python -c"from project.settings import DEBUG; print(DEBUG)"`
-dbname=`python -c"from project.settings import LOCAL_DATABASES; print(LOCAL_DATABASES['default']['NAME'])"`
+debug=$(python -c "from project.settings import DEBUG; print(DEBUG)")
+dbname=$(python -c "from project.settings import LOCAL_DATABASES; print(LOCAL_DATABASES['default']['NAME'])")
 
-if [ $debug = "True" ] ; then
+if [ "$debug" != "True" ] ; then
+  print_red "DEBUG is not True."
+  exit 1
+fi
+
 echo "----------------------drop-database------------------------------"
-    if [ $engine == "django.db.backends.sqlite3" ]; then
-        if [ -f $dbname ] ; then
-            echo "SQLITE: deleting $dbname"
-            rm $dbname
-        fi
-    else
-        dbuser=`python -c"from project.settings import LOCAL_DATABASES; print(LOCAL_DATABASES['default']['USER'])"`
-        dbpass=`python -c"from project.settings import LOCAL_DATABASES; print(LOCAL_DATABASES['default']['PASSWORD'])"`
-        if [ $engine == "django.db.backends.mysql" ]; then
-            echo "drop database $dbname" | mysql --user=$dbuser --password=$dbpass
-            echo "create database $dbname" | mysql --user=$dbuser --password=$dbpass
-        else
-            dropdb $dbname
-            createdb $dbname
-        fi
-    fi
-    python manage.py migrate
-fi
+dropdb "$dbname"
 
-if  $RUNSERVER ; then
-    python manage.py runserver 0.0.0.0:8000 --nothreading
-fi
+echo "  create-database"
+createdb "$dbname"
+
+python manage.py migrate
