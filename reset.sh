5fa18deb0af285fceea44e8c8334af86645d26e7
taandrighetti <taandrighetti@uc.cl>
2019-10-02 12:29:58 -0300

    chore: add back RUNSERVER to reset.sh

--- reset.sh
+++ reset.sh
@@ -5,6 +5,22 @@ function print_red() {
     echo -e "\033[31m$1\033[39m"
 }
 
+RUNSERVER=false
+while getopts "sh" OPTION
+do
+    case $OPTION in
+        s)
+             echo "Start Server"
+             RUNSERVER=true
+             ;;
+        ?)
+             echo "fail"
+             exit
+             ;;
+     esac
+done
+
+
 debug=$(python -c "from project.settings import DEBUG; print(DEBUG)")
 dbname=$(python -c "from project.settings import LOCAL_DATABASES; print(LOCAL_DATABASES['default']['NAME'])")
 
@@ -20,3 +36,8 @@ echo "  create-database"
 createdb "$dbname"
 
 python manage.py migrate
+
+
+if  $RUNSERVER ; then
+    python manage.py runserver 0.0.0.0:8000 --nothreading
+fi
