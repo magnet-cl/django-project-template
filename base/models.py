8ae8e331dff9cc3b7f1f62ffeaeb3a64a926a45a
Diego Alliende <diego.alliende@magnet.cl>
2019-11-14 10:40:34 -0300

    Update base models

--- base/models.py
+++ base/models.py
@@ -54,6 +54,18 @@ class BaseModel(AuditMixin, models.Model):
         verbose_name=_('updated at'),
     )
 
+    # field used to store a dictionary with the instance original fields
+    original_dict = None
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Disclaimer: Saving the many to many relations implies one more query
+        # for each many to many field of the model.
+        self.original_dict = self.to_dict(
+            exclude=['created_at', 'updated_at', 'original_dict', 'id'],
+            include_m2m=True,
+        )
+
     # using BaseManager
     objects = BaseManager()
 
