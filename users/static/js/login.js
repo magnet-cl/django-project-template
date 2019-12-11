ad7aff6aa8c0f5e1cee5ceb0b51d2dcfd33e84ad
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2019-12-11 15:02:22 -0300

    Add cookie watcher on login to avoid CSRF token errors
    When a users opens 2 tabs with the login, one login will fail, so we
    reload the page

--- users/static/js/login.js
+++ users/static/js/login.js
@@ -1,3 +1,36 @@
-window.onload = function() {
-  document.getElementById("id_email").focus();
-}
+window.onload = () => {
+  function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+      const cookies = document.cookie.split(';');
+      for (let i = 0; i < cookies.length; i += 1) {
+        const cookie = cookies[i].trim();
+
+        // Does this cookie string begin with the name we want?
+        if (cookie.substring(0, name.length + 1) === (`${name}=`)) {
+          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+          break;
+        }
+      }
+    }
+
+    return cookieValue;
+  }
+
+  const cookie = getCookie('csrftoken');
+
+  const interval = setInterval(() => {
+    if (cookie !== getCookie('csrftoken')) {
+      clearInterval(interval);
+      window.location.reload();
+    }
+  }, 1000);
+
+
+  const loginForm = document.getElementById('login-form');
+  const loginField = document.getElementById(loginForm.dataset.usernameFieldId);
+  if (loginField) {
+    loginField.focus();
+    loginField.select();
+  }
+};
