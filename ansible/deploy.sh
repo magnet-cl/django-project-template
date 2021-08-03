88d4d4372b08ed567e63194051af7556b2cbb347
taandrighetti <taandrighetti@uc.cl>
2021-08-03 14:26:06 -0400

    chore: add flag to skip production deploy tests

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -2,15 +2,25 @@
 set -e
 cd "$(dirname "$0")"
 
-if [[ -n $1 ]]; then
-  limit="$1"
-else
-  limit="remote"
-fi
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -f|--force)
+      force=1
+      ;;
+    *)
+      limit=$1
+      ;;
+  esac
+  shift
+done
 
 if [[ "$(basename "$0")" == "update.sh" ]]; then    # have you ever seen busybox?
   tags="--tags update"
 fi
 
+if [[ "$force" = "1" ]]; then
+  skip_tags="--skip-tags validate-deployment"
+fi
+
 # shellcheck disable=SC2086
-ansible-playbook --inventory inventory.yaml --limit "$limit" $tags playbooks/deploy.yaml
+ansible-playbook --inventory inventory.yaml --limit "${limit:-remote}" $tags $skip_tags playbooks/deploy.yaml
